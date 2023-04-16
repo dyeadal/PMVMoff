9f3575eea85afa1d2dedcc5f82e4ce8ff2cac5bc
Dyeadal <dyeadal@gmail.com>
2023-04-15 23:08:54 -0500

    Update pmvmoff.sh

--- pmvmoff.sh
+++ pmvmoff.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# creates vmid varaible that stores all VM IDs that have the status of "running"
-vmid=$(qm list | awk '$3 ~ /running/ {print $1}')
+# creates vmid varaible that stores all VM IDs present
+vmid=$(qm list | awk 'NR>1 {print $1}')
+
+echo $vmid
+
+# displays which ID line is running
+qm list | awk 'NR>1 {print $3}' | grep -n running
 
 # for loop that allows commands on individual lines=single VM ID
 for line in $vmid 
+
 do
         echo "VM: $line"
+
 done
